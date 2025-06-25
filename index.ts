eb3e05536c8ed8993cddcd19b1004b34b81585a0
AntiLox12 <devicebanned7@gmail.com>
2025-06-25 21:21:14 +0300

    Fix: use relative path for import

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { EventsSDK } from "github.com/octarine-public/wrapper/index"
+import { EventsSDK } from "../../octarine-public/wrapper/index"
 
 console.log("Hello world!")
 EventsSDK.on("GameStarted", () => {
